3c0303f42696cca3ba540c9db3934ac56b36eae7
Morgan <morgan.nott@hotmail.com>
2022-09-23 23:11:27 +0200

    Add getById method

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,3 @@
-
 import {
     Controller,
     Post,
@@ -9,12 +8,18 @@ import {
     Delete,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
+import {ApiBody, ApiCreatedResponse, ApiOperation, ApiTags} from "@nestjs/swagger";
+import {CreateUSerDto} from "./user.dto";
 
+@ApiTags('-- Users Module --')
 @Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) {}
 
     @Post()
+    @ApiOperation({ summary: 'Add new User' })
+    // @ApiCreatedResponse({ description: 'User Registration' })
+    // @ApiBody({ type: CreateUSerDto })
     async create(@Body('name') name: string,
                  @Body('email') email: string,
                  @Body('password') password: string) {
@@ -23,11 +28,20 @@ export class UsersController {
     }
 
     @Get()
+    @ApiOperation({ summary: 'get All Users' })
     async findAll(): Promise<any> {
         return this.usersService.findAll();
     }
 
+    @Get(':id')
+    @ApiOperation({ summary: 'Get User by ID' })
+    async findOne(@Param('id') userId: string): Promise<any> {
+        console.log('User', userId)
+        return this.usersService.findOne(userId);
+    }
+
     @Delete(':id')
+    @ApiOperation({ summary: 'Delete User by ID' })
     async deleteProduct(@Param('id') id: string) {
         return this.usersService.delete(id);
     }
